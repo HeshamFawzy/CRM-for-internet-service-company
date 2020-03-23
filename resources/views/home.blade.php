5a23905a03fc576c66031e8a554e5bcefa974ec6
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2020-03-23 19:22:13 +0200

    p19

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
 
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a><button
+        <a class="navbar-brand" href="{{ url('/')}}">CRM</a><button
             class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i
                 class="fas fa-bars"></i></button><!-- Navbar Search-->
         <!-- Navbar-->
@@ -82,7 +82,7 @@
                 </div>
                 <div class="sb-sidenav-footer">
                     <div class="small">Logged in as:</div>
-                    Start Bootstrap
+                    Admin
                 </div>
             </nav>
         </div>
