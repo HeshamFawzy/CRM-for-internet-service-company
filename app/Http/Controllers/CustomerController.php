5c721fc4d65201b61d41f7dc5773c0e11d3abbec
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2020-03-23 21:09:30 +0200

    p21

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,81 +4,106 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
+
+use App\Customer;
+
+
 class CustomerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    
+    public function viewcustomer()
     {
-        //
+        $Customers = DB::table('Customers')->get();
+        return view('customer.viewcustomer')->with(['Customers'] , $Customers);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function addcustomer()
     {
+        $Plans = DB::table('Plans')->get();
+        return view('customer.addcustomer')->with('options' , $Plans);
+    }
+
+    
+    public function addcustomerp(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'contact' => 'required',
+            'email' => 'required',
+            'age' => 'required',
+            'gender' => 'required',
+            'region' => 'required',
+        ]);
+
+        $Customer = Customer::create([
+            'name' => $request->input('name'),
+            'contact' => $request->input('contact'),
+            'email' => $request->input('email'),
+            'age' => $request->input('age'),
+            'gender' => $request->input('gender'),
+            'region' => $request->input('region'),
+        ]);
+        if($Customer->save()){
+            return redirect('addcustomer')->with('message', 'Added Successfully!');
+        }
         return view('customer.addcustomer');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    
+    public function editcustomer($id)
     {
-        //
+        $Customer = DB::table('Customers')->where('id', $id)->first();
+        return view('customer.editcustomer')->with('Customer', $Customer);
+    } 
+    
+    public function editcustomerp(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'contact' => 'required',
+            'email' => 'required',
+            'age' => 'required',
+            'gender' => 'required',
+            'region' => 'required',
+        ]);
+
+        $Customer = Customer::where('id' , '=' , $request->input('id'))->update([
+            'name' => $request->input('name'),
+            'contact' => $request->input('contact'),
+            'email' => $request->input('email'),
+            'age' => $request->input('age'),
+            'gender' => $request->input('gender'),
+            'region' => $request->input('region'),
+        ]);
+        if($Customer = 1){
+            return redirect('viewcustomer')->with('message', 'Updated Successfully!');
+        }
+        return view('customer.viewcustomer');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+   
+    public function deletecustomer($id)
     {
-        //
+        $Customer = Customer::where('id', $id)->first();
+        if($Customer != null){
+            $Customer->delete();
+        }
+        if($Customer = 1){
+            return redirect('viewcustomer')->with('message', 'Deleted Successfully!');
+        }
+        return view('customer.viewcustomer');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function searchcustomer(Request $request)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $Customers = Customer::Where('name', 'like' , '%'.$request->input('query').'%')
+        ->orwhere('contact', 'like' , '%'.$request->input('query').'%')
+        ->orWhere('email', 'like' , '%'.$request->input('query').'%')
+        ->orWhere('region', 'like' , '%'.$request->input('query').'%')
+        ->orderBy('created_at', 'desc')
+        ->get();
+        return view('customer.viewcustomer')->with('Customer' , $Customer);
     }
 }
--- resources/views/customer/addcustomer.blade.php
+++ resources/views/customer/addcustomer.blade.php
@@ -11,7 +11,7 @@
   <div class="card-body">
     <form
       method="post"
-      action="{{ route('addcustomer') }}"
+      action="{{ route('addcustomerp') }}"
       enctype="multipart/form-data"
     >
       {{ csrf_field() }}
@@ -57,12 +57,7 @@
       </div>
       <div class="form-group">
         <label for="gender"><strong>Customer Gender :</strong><br /></label>
-        <select
-          id="bloodgroup"
-          class="form-control"
-          name="bloodgroup"
-          required=""
-        >
+        <select id="gender" class="form-control" name="gender" required="">
           <option value="" disabled="disabled" selected="true"
             >Select Gender</option
           >
@@ -79,6 +74,32 @@
           name="region"
           required
         />
+      </div>
+      <div class="form-group">
+        <label for="business"><strong>Customer Business :</strong><br /></label>
+        <select id="business" class="form-control" name="business" required="">
+          <option value="" disabled="disabled" selected="true"
+            >Select Business</option
+          >
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="plan"><strong>Customer Plan :</strong><br /></label>
+        <select
+          id="plan"
+          class="form-control"
+          name="plan"
+          required=""
+        >
+          <option value="" disabled="disabled" selected="true"
+            >Select Plan</option
+          >
+          @if($options ?? '') @foreach($options as $option)
+          <option value="{{ $option->name }}">{{ $option->name }} </option>
+          @endforeach @endif
+        </select>
       </div>
       <div class="form-group text-center">
         <button
