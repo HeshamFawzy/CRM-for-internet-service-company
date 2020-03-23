632caeae1605b96845143dc63759e80769a73deb
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2020-03-23 13:06:17 +0200

    p13

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,10 @@
                 <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                    <a class="dropdown-item" href="login.html">Logout</a>
+                <form action="{{ route('logout') }}" method="POST">
+                        {{ csrf_field() }}
+                        <button type="submit" class="dropdown-item">Logout</button>
+                    </form>
                 </div>
             </li>
         </ul>
